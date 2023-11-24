46560d5567290b50f6cbff64d6adb2aabcc2a8aa
Akayiz <ahmed.khaled.yousry@gmail.com>
2023-11-25 00:52:31 +0200

    Upsolved B in div2 899

--- codeforces/contests/codeforces_div2_899/B.cpp
+++ codeforces/contests/codeforces_div2_899/B.cpp
@@ -1,6 +1,6 @@
 /**
 *    author:  Akayiz
-*    created: 2023-09-25 17:47:54
+*    created: 2023-11-24 22:41:34
 **/
 
 #include <bits/stdc++.h>
@@ -18,14 +18,64 @@ int main() {
     int t;
     cin >> t;
 
-    int n, k;
+    int k, si, n;
     while(t--){
         cin >> n;
-        for (int i = 0; i < n; i++)
-        {
+        set<vector<int>> s = {};
+        int freq[55] = {};
+        while(n--){
             cin >> k;
+            vector<int> v = {};
+            for (int i = 0; i < k; i++)
+            {
+                cin >> si;
+                v.push_back(si);
+            }
+            s.insert(v);
         }
-    }
 
+        int unique_digits_count = 0;
+
+        for (auto i : s)
+        {
+            for (auto j : i)
+            {
+                if(freq[j] == 0)
+                    unique_digits_count++;
+                freq[j]++;
+            }
+        }
+
+        int ans = 0, temp_freq[55] = {};
+        
+        int digits_removed;
+        for (int x = 1; x < 51; x++)
+        {
+            digits_removed = 0;
+            for (int i = 0; i < 51; i++)
+            {
+                temp_freq[i] = freq[i];
+            }
+            for (auto i : s)
+            {
+                for (auto j : i)
+                {
+                    if(j == x){
+                        for (auto k : i)
+                        {
+                            temp_freq[k]--;
+                            digits_removed += !temp_freq[k];
+                        }
+                        break;
+                    }
+                }
+            }
+            if(digits_removed != 0)
+                ans = max(ans, unique_digits_count - digits_removed);
+        }
+
+        cout << ans << el;
+    }
+    
     return 0;
 }
